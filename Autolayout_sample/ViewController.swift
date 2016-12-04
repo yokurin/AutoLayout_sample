34d185d295332245bbc16a988fa698eda4c88598
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2016-12-04 23:14:58 +0900

    Initial Commit

---
+++ Autolayout_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout_sample
+//
+//  Created by Hayashi Tsubasa on 12/4/16.
+//  Copyright © 2016 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
